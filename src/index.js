94701d2f2166d6ed2a85ae2dbf5c344ca4d1e2df
MaryBayt <94868308+MaryBayt@users.noreply.github.com>
2023-02-23 13:58:58 +0200

    init: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newStr = str;
+  let i = 0;
+  function deletePairs() {
+    bracketsConfig.forEach((br) => {
+      newStr = newStr.replace(`${br[0]}${br[1]}`, '');
+    });
+  };
+
+  do {
+    deletePairs();
+    i++;
+  } while (i < 100);
+
+  let result = (newStr.length === 0) ? true : false;
+  return result;
 }
